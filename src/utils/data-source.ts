ad5bf6a37e293291439223b1beefdc64e3a9bac7
Ashikam263 <ashikam263@gmail.com>
2024-05-16 12:21:23 +0530

    added ormconfig inside

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -1,22 +1,47 @@
-require('dotenv').config();
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import config from 'config';
+// require('dotenv').config();
+// import 'reflect-metadata';
+// import { DataSource } from 'typeorm';
+// import config from 'config';
 
-const postgresConfig = config.get<{
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-}>('postgresConfig');
+// const postgresConfig = config.get<{
+//   host: string;
+//   port: number;
+//   username: string;
+//   password: string;
+//   database: string;
+// }>('postgresConfig');
+
+// export const AppDataSource = new DataSource({
+//   ...postgresConfig,
+//   type: 'postgres',
+//   synchronize: false,
+//   logging: false,
+//   entities: ['src/entities/**/*.entity{.ts,.js}'],
+//   migrations: ['src/migrations/**/*{.ts,.js}'],
+//   subscribers: ['src/subscribers/**/*{.ts,.js}'],
+// });
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "../entities/user.entity"
+
+
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } = process.env;
+
 
 export const AppDataSource = new DataSource({
-  ...postgresConfig,
-  type: 'postgres',
-  synchronize: false,
-  logging: false,
-  entities: ['src/entities/**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/**/*{.ts,.js}'],
-  subscribers: ['src/subscribers/**/*{.ts,.js}'],
-});
+    type: "postgres",
+    host: DB_HOST,
+    port: parseInt(DB_PORT || "5432"),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    synchronize: NODE_ENV === "development" ? true : false,
+    logging: NODE_ENV === "development" ? false : false,
+    entities: [User,],
+    migrations: [__dirname + "/migration/*.ts"],
+    subscribers: [],
+})
