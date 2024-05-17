2da7e983deb959998b8ccf00ce9d571bf9660f3b
Ashikam263 <ashikam263@gmail.com>
2024-05-17 11:27:32 +0530

    works on desrialize userr

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -5,6 +5,76 @@ import AppError from '../utils/appError';
 // import redisClient from '../utils/connectRedis';
 import { verifyJwt } from '../utils/jwt';
 import jwt from 'jsonwebtoken';
+import time from '../config/time';
+
+// export const deserializeUser = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     let access_token;
+
+//     if (
+//       req.headers.authorization &&
+//       req.headers.authorization.startsWith('Bearer')
+//     ) {
+//       access_token = req.headers.authorization.split(' ')[1];
+//     } else if (req.cookies.access_token) {
+//       access_token = req.cookies.access_token;
+//     }
+
+//     if (!access_token) {
+//       return next(new AppError(401, 'You are not logged in'));
+//     }
+
+//     // Validate the access token
+
+//     // const decoded = verifyJwt<{ sub: string }>(
+//     //   access_token,
+//     //   'accessTokenPublicKey'
+//     // );
+//     const decoded = jwt.verify(access_token, 'accessTokenPublicKey');
+
+//     if (!decoded) {
+//       return next(new AppError(401, `Invalid token or user doesn't exist`));
+//     }
+
+//     // Check if the user has a valid session
+//     // const session = await redisClient.get(decoded.sub);
+
+//     // if (!session) {
+//     //   return next(new AppError(401, `Invalid token or session has expired`));
+//     // }
+
+//     // const session = await jwt.verify(decoded.sub, 'refreshTokenPublicKey');
+//     // const session = await verifyJwt<{ sub: string }>(
+//     //   access_token,
+//     //   'accessTokenPublicKey'
+//     // );
+//     const session = await jwt.verify(access_token, 'accessTokenPublicKey');
+
+//     if (!session) {
+//       return next(new AppError(401, `Invalid token or session has expired`));
+//     }
+
+//     // Check if the user still exist
+//     // const user = await findUserById(JSON.parse(session).id);
+//     const user = await findUserById(decoded.sub as string);
+
+//     if (!user) {
+//       return next(new AppError(401, `Invalid token or session has expired`));
+//     }
+
+//     // Add user to res.locals
+//     res.locals.user = user;
+
+//     next();
+//   } catch (err: any) {
+//     next(err);
+//   }
+// };
+
 
 export const deserializeUser = async (
   req: Request,
@@ -27,46 +97,38 @@ export const deserializeUser = async (
       return next(new AppError(401, 'You are not logged in'));
     }
 
-    // Validate the access token
-    const decoded = verifyJwt<{ sub: string }>(
-      access_token,
-      'accessTokenPublicKey'
-    );
+    // Retrieve JWT public key from environment variables
+    const accessTokenPublicKey = process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY;
+    if (!accessTokenPublicKey) {
+      return next(new AppError(500, 'JWT public key not configured'));
+    }
 
+    // Validate the access token
+    const decoded = jwt.verify(access_token, accessTokenPublicKey) as { sub: string };
+    console.log('Decoded Token:', decoded);
     if (!decoded) {
       return next(new AppError(401, `Invalid token or user doesn't exist`));
     }
 
-    // Check if the user has a valid session
-    // const session = await redisClient.get(decoded.sub);
-
-    // if (!session) {
-    //   return next(new AppError(401, `Invalid token or session has expired`));
-    // }
-
-    // const session = await jwt.verify(decoded.sub, 'refreshTokenPublicKey');
-    const session = await verifyJwt<{ sub: string }>(
-      access_token,
-      'accessTokenPublicKey'
-    );
-
-    if (!session) {
-      return next(new AppError(401, `Invalid token or session has expired`));
-    }
-
-    // Check if the user still exist
-    // const user = await findUserById(JSON.parse(session).id);
+    // Check if the user still exists
     const user = await findUserById(decoded.sub);
 
     if (!user) {
-      return next(new AppError(401, `Invalid token or session has expired`));
+      return next(new AppError(401, `User not found`));
     }
 
-    // Add user to res.locals
+    // Add user to res.locals for use in subsequent middleware/routes
     res.locals.user = user;
 
     next();
   } catch (err: any) {
+    // Handle JWT verification errors
+    if (err.name === 'TokenExpiredError') {
+      return next(new AppError(401, 'Token has expired'));
+    } else if (err.name === 'JsonWebTokenError') {
+      return next(new AppError(401, 'Invalid token this is issue'));
+    }
+
     next(err);
   }
 };
