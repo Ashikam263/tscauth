7341129c5b70d39f6c7a333da312947a2fe5bd46
Ashikam263 <ashikam263@gmail.com>
2024-05-17 21:11:32 +0530

    public

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,8 +1,24 @@
+// export default class AppError extends Error {
+//   status: string;
+//   isOperational: boolean;
+//   constructor(public statusCode: number = 500, public message: string) {
+//     super(message);
+//     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+//     this.isOperational = true;
+
+//     Error.captureStackTrace(this, this.constructor);
+//   }
+// }
+
 export default class AppError extends Error {
-  status: string;
-  isOperational: boolean;
-  constructor(public statusCode: number = 500, public message: string) {
+  public statusCode: number;
+  public status: string;
+  public isOperational: boolean;
+
+  constructor(statusCode: number, message: string) {
     super(message);
+
+    this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
