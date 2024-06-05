d8499d1a066f71c95b070b3b7e78ba517a71a8d2
Ashikam263 <ashikam263@gmail.com>
2024-06-05 22:37:47 +0530

    expiresIn 5173

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
-export default {  origin: 'http://localhost:3000',
+export default {  origin: 'http://localhost:5173',
   accessTokenExpiresIn: 15,
   refreshTokenExpiresIn: 60,
   emailFrom: 'ashikam263@gmail.com',
